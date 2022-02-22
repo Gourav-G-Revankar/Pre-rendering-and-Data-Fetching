5721589079a14945eb3e0fdfbb546b02d63030a0
Gourav-G-Revankar <gourav.revankar35@gmail.com>
2022-02-22 21:45:08 +0530

    working on pre-rendering examples

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,25 @@
+import Link from 'next/link'
+
 function Home() {
-  return(
-    <h1>Next JS Pre-rendering</h1>
+  return (
+    <>
+      <h1>Next JS Pre-rendering</h1>
+      <Link href="/users">
+        <a>Users</a>
+      </Link>
+      <br />
+      <Link href="/posts">
+        <a>Post</a>
+      </Link>
+      <br />
+      <Link href="/news">
+        <a>News</a>
+      </Link>
+      <br />
+      <Link href="/dashboard">
+        <a>Dashboard</a>
+      </Link>
+    </>
   )
 }
 
